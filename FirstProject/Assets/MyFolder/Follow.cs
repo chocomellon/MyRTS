e744a89fd0e038446aa608a1cc20c51b0c6e988b
Mellon <kappa_sense_pen@yahoo.co.jp>
2015-11-30 04:07:21 +0900

    ズームイン・アウト可能、ターゲットを回転無しで追うカメラの実装

---
+++ FirstProject/Assets/MyFolder/Follow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour
+{
+
+    public GameObject objTarget;
+    public Vector3 offset;
+    private Vector3 fWheelPos;
+
+    void Start()
+    {
+        updatePostion();
+    }
+
+    void LateUpdate()
+    {
+        updatePostion();
+
+    }
+
+    void updatePostion()
+    {
+        Vector3 pos = objTarget.transform.localPosition;
+        float fWheel = Input.GetAxis("Mouse ScrollWheel");
+        Vector3 fWheelpos=new Vector3 (0, 0, fWheel*4);
+        fWheelPos += fWheelpos;
+        transform.localPosition = pos + offset+fWheelPos;
+    }
+}
---
+++ FirstProject/Assets/MyFolder/MouseControlCamera.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseControlCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float distance = 1.0f;
+
+    //追加
+    [SerializeField]
+    private float zMin;  //マウスホイールで近づける最小距離
+    [SerializeField]
+    private float zMax; //マウスホイールで離れられる最大距離
+
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    Vector3 angles;
+    void Start()
+    {
+        angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+    }
+
+
+    void LateUpdate()
+    {
+        if (target)
+        {
+
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+            Vector3 position = rotation * new Vector3(0.0f, 0.0f, -distance*10) + target.position;
+
+            transform.rotation = rotation;
+            transform.position = position;
+
+
+            //以下追加
+            //ズームの距離に制限を設ける
+            distance -= Input.GetAxis("Mouse ScrollWheel");
+            distance = Mathf.Clamp(distance, zMin, zMax);
+
+
+        }
+    }
+
+}
+
