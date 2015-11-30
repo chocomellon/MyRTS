08670e4a9f2a16f8c32ff7cfd9192fc5250ea73f
Mellon <kappa_sense_pen@yahoo.co.jp>
2015-11-30 20:10:56 +0900

    アセット追加しすぎた

---
+++ FirstProject/Assets/MyFolder/PlayerMove.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(NavMeshAgent), typeof(Animator))]
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField, HideInInspector]
+    NavMeshAgent agent;
+    [SerializeField, HideInInspector]
+    Animator animator;
+
+    private RaycastHit hit;
+    private Ray ray;
+
+    void Reset()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        animator.SetFloat("Speed", agent.velocity.sqrMagnitude);
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
